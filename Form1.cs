1d89d9a03afccc6865eef8015fc313bd552053f1
busramerveaydn <32596414+busramerveaydn@users.noreply.github.com>
2020-08-19 17:57:45 +0300

    Create Form1.cs
    Masaüstü uygulaması ile dama tahtası

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecapDemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] buttons = new Button[8, 8];//array ile
+            //8 satır için bir değer oluşturduk
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i <= buttons.GetUpperBound(0); i++)//buradaki getupperbound ,0.boyutun en büyük alabileceği değer 7 dir
+            {
+                for (int j = 0; j <= buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 60;
+                    buttons[i, j].Height = 60;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 60;
+
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                    this.Controls.Add(buttons[i, j]);
+                }
+                top += 60;
+                left = 0;
+            }
+
+
+
+        }
+    }
+}
